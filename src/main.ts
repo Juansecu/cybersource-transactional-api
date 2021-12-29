a0004a0cbd2a2534a34ba5886358cbad673d7e3d
Juansecu <juansecoder@hotmail.com>
2021-12-29 13:19:07 -0500

    CORS access was enabled and ValidationPipe was enabled globally

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 /* --- Third-party libraries --- */
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 /* --- Modules --- */
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
