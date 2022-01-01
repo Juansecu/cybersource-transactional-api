2c11999bf69780806d80034725ed364193f91904
Juansecu <juansecoder@hotmail.com>
2021-12-31 20:45:35 -0500

    Checking for token expiration on UserAuthenticationMiddleware was fixed

--- src/users/middlewares/user-authentication.middleware.ts
+++ src/users/middlewares/user-authentication.middleware.ts
@@ -3,12 +3,12 @@ import {
   Injectable,
   InternalServerErrorException,
   NestMiddleware,
+  NotFoundException,
   UnauthorizedException
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { NextFunction, Request, Response } from 'express';
-import { verify } from 'jsonwebtoken';
-import { Repository } from 'typeorm';
+import { decode } from 'jsonwebtoken';
 
 /* --- DTOs --- */
 import { MessageResDto } from '../../shared/dtos/responses/message.res-dto';
@@ -16,17 +16,20 @@ import { MessageResDto } from '../../shared/dtos/responses/message.res-dto';
 /* --- Entities --- */
 import { UserEntity } from '../entities/user.entity';
 
+/* --- Repositories --- */
+import { UsersEntityRepository } from '../repositories/entity-repositories/users.entity-repository';
+
 @Injectable()
 export class UserAuthenticationMiddleware implements NestMiddleware {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly _USERS_REPOSITORY: Repository<UserEntity>
+    private readonly _USERS_REPOSITORY: UsersEntityRepository
   ) {}
 
   async use(request: Request, response: Response, next: NextFunction) {
     if (
-      !request.headers.authorization &&
-      request.headers.authorization.split(' ').length !== 2 &&
+      !request.headers.authorization ||
+      request.headers.authorization.split(' ').length !== 2 ||
       request.headers.authorization.split(' ')[0] != 'Bearer'
     )
       throw new UnauthorizedException(
@@ -34,11 +37,16 @@ export class UserAuthenticationMiddleware implements NestMiddleware {
       );
 
     const token = request.headers.authorization.split(' ')[1];
-    const payload = verify(token, process.env.JWT_SECRET);
+    const payload = decode(token);
 
     if (!payload)
       throw new UnauthorizedException(
         new MessageResDto(false, 'Invalid token')
+      );
+
+    if (Date.now() >= (payload as any).exp * 1000)
+      throw new UnauthorizedException(
+        new MessageResDto(false, 'Token expired')
       );
 
     try {
@@ -54,9 +62,7 @@ export class UserAuthenticationMiddleware implements NestMiddleware {
         };
         next();
       } else
-        throw new UnauthorizedException(
-          new MessageResDto(false, 'Unkown user')
-        );
+        throw new NotFoundException(new MessageResDto(false, 'Unkown user'));
     } catch (error) {
       throw new InternalServerErrorException(
         new MessageResDto(false, 'Internal server error')
